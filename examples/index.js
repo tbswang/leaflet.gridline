56271fca1b00bc69c989010ab91b1cf40e9d995b
tbswang <gt2017qd@163.com>
2020-04-17 11:44:40 +0800

    update example

--- examples/index.js
+++ examples/index.js
@@ -2,9 +2,18 @@ import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
 import '../src/index';
+// import "leaflet.gridline";
 
 const mymap = L.map('mapid').setView([39.9, 116.4], 15);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mymap);
 
-L.gridlineLayer({ geohashLength: 6 }).addTo(mymap);
+const layer = L.gridlineLayer({ geohashLength: 6 });
+
+window.addGrid = function(){
+  layer.addTo(mymap);
+}
+
+window.removeGrid = function(){
+  layer.remove();
+}
